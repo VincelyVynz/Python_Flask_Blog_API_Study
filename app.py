d2f8ef562550b88f80c6e9956bb63116a0f23a75
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-10 15:08:59 +0530

    Add get posts method to app.py

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def index():
     return 'Welcome to the Blog!'
 
+@app.route('/posts')
+def get_posts():
+
+    conn = sqlite3.connect('blog.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM posts')
+    posts = cursor.fetchall()
+    posts_list = []
+    for row in posts:
+        posts_list.append({'id': row[0], 'title': row[1], 'content': row[2], 'author': row[3], 'timestamp': row[4]})
+    return jsonify(posts_list)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
